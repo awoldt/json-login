4927d68775386146e27b53f9bc627f4787cc4947
awoldt <48081351+awoldt@users.noreply.github.com>
2020-06-13 11:31:01 -0400

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,29 +1,48 @@
 import json
 
 def createAcct():
-    fname = input("first name: ")
-    lname = input("last name: ")
-    age = input("age: ")
+    userAttributes = ("first name: ","last name: ","email: ")
+    attributesIndex = 0
 
-    # assigns user values to be stored in json file
+    def validateInput(x):
+        error = True
+        while(error == True):
+            if(len(x) == 0):
+                print("\n>>error: must enter value\n")
+                x = input(userAttributes[attributesIndex])
+            else:
+                return x
+                error = False
+                    
+    fname = input("first name: ")
+    fname = validateInput(fname)
+    lname = input("last name: ")
+    lname = validateInput(lname)
+    email = input("email: ")
+    email = validateInput(email)
+
+
+    # assigns user values into python dict
     userProfile = {
         "fname": fname,
         "lname": lname,
-        "age": age
+        "email": email
     }
 
-    # gets data from data.json
+    # gets json from data.json
     getJsonData = open("/users/alex/documents/data.json", "r")
     jsonStr = getJsonData.read() # returns raw json string
     pythonObj = json.loads(jsonStr) #converts json string into python dict
     pythonObj["users"].append(userProfile)
-
+    # appends new json data to data.json
     appendJsonData = open("/users/alex/documents/data.json", "w")
-    x = json.dumps(pythonObj)
+    x = json.dumps(pythonObj, indent=4)
     appendData = appendJsonData.write(x)
 
     getJsonData.close()
     appendJsonData.close()
+
+    print("\naccount created!\n")
 
 
 
--- login2.py
+++ login2.py
@@ -1,10 +1,12 @@
 import functions
 
 loop = True
+print("\n")
 while(loop == True):
     userCommand = input("create, edit or delete account: ")
+    userCommand = userCommand.lower()
     if userCommand == "create":
         userData = functions.createAcct() #fname, lname, and email info returned
     else:
-        print("error")
+        print("\n>>error: unknown command '" + userCommand + "'\n")
 
